310ba4f6457a01ddbc265c82afae5b6d133b9af5
devincloudkelly <devin.cloud.kelly@gmail.com>
2019-11-08 11:17:12 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,26 @@
 class Dog
+    attr_accessor :name, :breed, :id
 
-    attr_accessor :name, :breed
-    attr_reader :id
+    def initialize(hash)
+       hash.each {|k, v| self.send(("#{k}="), v)}
+    end
 
-    def initialize(name:, breed:, id: nil)
-        @name = name
-        @breed = breed
-        @id = id
+
+    ## be sure you save your id back to the object instance
+    ## otherwise you'll spend too much time trying to get 
+    ## .find_or_create_by to pass ;)
+    def save
+        if  self.id
+            self.update
+        else
+            sql =<<-SQL 
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+            SQL
+            DB[:conn].execute(sql, self.name, self.breed)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
+        self
     end
 
     def self.create_table
@@ -19,8 +33,54 @@ class Dog
         DB[:conn].execute(sql)
     end
 
-    def save(name:, breed:)
-        dog = Dog.new(name, breed)
-        dog
+    # def self.new_from_db(array)
+    #     dog = Dog.new(id: array[0], name: array[1], breed: array[2])
+  
+    # end
+
+    def self.new_from_db(row)
+        id = row[0]
+        name = row[1]
+        breed = row[2]
+        self.new(id: id, name: name, breed: breed)
     end
+
+
+    def self.create(hash)
+        dog = Dog.new(hash)
+        dog.save
+    end
+
+    def self.find_by_id(id)
+        sql = <<-SQL
+            SELECT * FROM dogs
+            WHERE id = ?
+        SQL
+        arr = DB[:conn].execute(sql, id)[0]
+        Dog.new_from_db(arr)
+    end
+    
+    def self.find_or_create_by(hash)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", hash[:name], hash[:breed])
+        if !dog.empty?
+            dog_data = dog[0]
+            new_dog = Dog.new_from_db(dog_data)
+        else
+            newer_dog = self.create(hash)
+        end
+        # binding.pry
+    end
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+        dog = DB[:conn].execute(sql, name)[0]
+        Dog.new_from_db(dog)
+        # binding.pry
+    end 
+
+    def update
+       sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+       DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
 end
