0db7f89b8ab9abbfcaed20167e1779b3e91c11fc
devincloudkelly <devin.cloud.kelly@gmail.com>
2019-11-07 17:55:38 -0800

    partway done

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(name:, breed:, id: nil)
+        @name = name
+        @breed = breed
+        @id = id
+    end
+
+    def self.create_table
+        sql = "CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)"
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = "DROP TABLE dogs"
+        DB[:conn].execute(sql)
+    end
+
+    def save(name:, breed:)
+        dog = Dog.new(name, breed)
+        dog
+    end
+end
